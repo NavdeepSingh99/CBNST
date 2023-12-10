4074eb07251241ae32ffcf8cc8c1eaeb8ea0d237
NavdeepSingh99 <132869610+NavdeepSingh99@users.noreply.github.com>
2023-12-10 19:12:53 +0530

    Update p5.c

--- p5.c
+++ p5.c
@@ -19,16 +19,18 @@ float iteration_method(double x0, double x1){
 
     double  x;
     int i = 1;
-    while(fabs(gh(x0)) < 1 && fabs(gh(x1)) < 1){
-
-        x1 = g(x0);
-
-        printf("iteration %d result = %lf\n",i, x1);i++;
-        
-        x = x0;
-        x0 = x1;
-
-        if(fabs(x0-x) <= 0.0001) break;
+    if(fabs(gh(x0)) < 1 && fabs(gh(x1)) < 1){
+        while(1){
+    
+            x1 = g(x0);
+    
+            printf("iteration %d result = %lf\n",i, x1);i++;
+            
+            x = x0;
+            x0 = x1;
+    
+            if(fabs(x0-x) <= 0.0001) break;
+        }
     }
 
     return x1;
